7c7681ca473e23109b145888a8298ecad69a0097
Emmanuel Otabil <emmazola9@gmail.com>
2020-11-08 09:02:09 +0000

    python practice

--- Num_square.py
+++ Num_square.py
@@ -3,8 +3,7 @@ result = {}
 num = int(input('How many squares will you like to determine: '))
 
 for key in range(1,num+1):
-    value = key*key
-    result[key]=value
+    result[key] = key * key
 
 for key,value in result.items():
     print('\n-',key,'----',value)
