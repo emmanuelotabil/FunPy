11d7d9d1febf50f3fed0d9b1c888847f5c245e41
Emmanuel Otabil <emmazola9@gmail.com>
2020-11-08 09:15:54 +0000

    python practice

--- Factorial.py
+++ Factorial.py
@@ -1,6 +1,10 @@
+print('\t\tIn this program, we will determine the factorial of some numbers.')
+
 def fact(x):
     if x == 0:
         return 1
     return x * fact(x - 1)
-x=int(input('Enter a number: '))
+
+x=int(input('Enter a number to determine its factorial: '))
+
 print (fact(x))
