df476bcab58ca85935b27d97073991506140942b
Emmanuel Otabil <emmazola9@gmail.com>
2020-11-08 03:24:47 +0000

    python practice

--- Divisibility_by_7.py
+++ Divisibility_by_7.py
@@ -3,3 +3,13 @@ number = range(2000,3201)
 for num in number:
     if num % 7 ==0 and num % 5 != 0:
         print('-',num)
+
+
+#ALTERNATIVE SOLUTION
+output = []
+
+for i in range(2000,3201):
+    if i % 7 == 0 and i % 5 != 0:
+        output.append(str(i))
+
+print('\n\n\n',','.join(output))
