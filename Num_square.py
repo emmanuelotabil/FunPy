7c36362ed8b97f12c54af6943c4b37139beaa0b3
Emmanuel Otabil <emmazola9@gmail.com>
2020-11-08 09:06:14 +0000

    python practice

--- Num_square.py
+++ Num_square.py
@@ -1,8 +1,13 @@
 result = {}
 
-num = int(input('How many squares will you like to determine: '))
+print('\t\tIn this program, we will print out the squares of numbers in a given range.')
 
-for key in range(1,num+1):
+num_1 = int(input('\n\nEnter lower limit of range: '))
+
+num_2 = int(input('\n\nEnter upper limit of range: '))
+
+
+for key in range(num_1,num_2+1):
     result[key] = key * key
 
 for key,value in result.items():
