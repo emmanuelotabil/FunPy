7af432c7ff4c47f3bb638fa51ec8edf0f622beb1
Emmanuel Otabil <emmazola9@gmail.com>
2020-11-09 23:41:44 +0000

    python practice

--- List_tuple.py
+++ List_tuple.py
@@ -1,9 +1,14 @@
+
+#Get user input
 values = input('Enter your values: ')
 
+#Convert input to list
 lst = values.split(',')
 
+#convert list to tuple
 tup = tuple(lst)
 
+#Print both list and tuple
 print(lst)
 
 print(tup)
