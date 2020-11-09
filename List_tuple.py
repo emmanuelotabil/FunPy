f27b7e202079b141b0de0daf6d0a3dfe6db7df6a
Emmanuel Otabil <emmazola9@gmail.com>
2020-11-09 23:43:02 +0000

    python practice

--- List_tuple.py
+++ List_tuple.py
@@ -1,3 +1,4 @@
+#Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains every number.
 
 #Get user input
 values = input('Enter your values: ')
