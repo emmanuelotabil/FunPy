ed7f4d04f59f752fbc31c4a016f3cd7e4c5dddb9
Emmanuel Otabil <emmazola9@gmail.com>
2020-11-09 23:44:14 +0000

    python practice

--- List_tuple.py
+++ List_tuple.py
@@ -1,7 +1,7 @@
 #Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains every number.
 
 #Get user input
-values = input('Enter your values: ')
+values = input('Enter your comma separated values to be converted: ')
 
 #Convert input to list
 lst = values.split(',')
