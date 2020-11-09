0934d613f3357c39082afa074906ace714f24008
Emmanuel Otabil <emmazola9@gmail.com>
2020-11-09 23:56:55 +0000

    python practice

--- Word_sort.py
+++ Word_sort.py
@@ -1,5 +1,9 @@
+#Write a program that accepts a comma separated sequence of words as input and prints the words in a comma-separated sequence after sorting them alphabetically.
+
+#Take user input of comma separated words
 words = input('Enter your comma separated words: ')
 
 words.split(',').sort()
 
+#print outcome
 print(''.join(words))
