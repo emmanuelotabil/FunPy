4e5f57c3a9a1274977d7918eaaefec8765317e1a
Restyled.io <commits@restyled.io>
2020-11-10 00:08:44 +0000

    Restyled by autopep8

--- Word_sort.py
+++ Word_sort.py
@@ -1,9 +1,9 @@
-#Write a program that accepts a comma separated sequence of words as input and prints the words in a comma-separated sequence after sorting them alphabetically.
+# Write a program that accepts a comma separated sequence of words as input and prints the words in a comma-separated sequence after sorting them alphabetically.
 
-#Take user input of comma separated words
+# Take user input of comma separated words
 words = input('Enter your comma separated words: ')
 
 words.split(',').sort()
 
-#print outcome
+# print outcome
 print(''.join(words))
