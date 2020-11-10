fb6f69e4a80fa6b635ec32708d455b78684aea61
Emmanuel Otabil <emmazola9@gmail.com>
2020-11-10 23:54:38 +0000

    python practice

--- Divisibility_by_5.py
+++ Divisibility_by_5.py
@@ -1,6 +1,6 @@
 values = []
 
-numbers = input('Enter your numbers: ')
+numbers = input('Enter comma separated binary numbers: ')
 
 numbers = numbers.split(',')
 
@@ -10,4 +10,4 @@ for num in numbers:
     if int_num % 5==0:
         values.append(num)
 
-print(values)
+print('The binary number(s) which is/are divisible by 5 when converted to decimal are: ',values)
