17ce2816b7664e59678e442b41ccf4c732c3263b
Emmanuel Otabil <emmazola9@gmail.com>
2020-11-10 23:56:40 +0000

    python practice

--- Divisibility_by_5.py
+++ Divisibility_by_5.py
@@ -1,9 +1,13 @@
+
+#Create empty ist to store values
 values = []
 
+#Get user input of comma separated binary numbers
 numbers = input('Enter comma separated binary numbers: ')
 
 numbers = numbers.split(',')
 
+#Check their divisibility by 5
 for num in numbers:
     x = str(num)
     int_num = int(x,2)
