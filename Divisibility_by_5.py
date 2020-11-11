17ce2f92b9e6157319425e8fdfd055616c2677f2
Emmanuel Otabil <emmazola9@gmail.com>
2020-11-11 00:00:08 +0000

    python practice

--- Divisibility_by_5.py
+++ Divisibility_by_5.py
@@ -1,3 +1,6 @@
+#Write a program which accepts a sequence of comma separated 4 digit binary numbers as its input and then check whether they are divisible by 5 or not. The numbers that are divisible by 5 are to be printed in a comma separated sequence.
+
+
 
 #Create empty ist to store values
 values = []
